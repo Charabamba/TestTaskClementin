786f2f1044aa1204464d93a4d6ee4694d90ae58a
Charabamba <54855818+Charabamba@users.noreply.github.com>
2020-03-23 16:41:32 +0500

    markup fix

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,40 @@
 let submitButton = document.querySelector(".submit-button");
+let permission = document.getElementById("checkbox-permission");
+
+permission.addEventListener("click", e => {
+  permission.checked ? (submitButton.disabled = "") : 0;
+});
 
 submitButton.addEventListener("click", e => {
   e.preventDefault();
-  let name = document.getElementById("name").value;
+  let requiredInputs = document.querySelectorAll(".form__input");
+  requiredInputs.forEach(elem => {
+    if (elem.value === "" && !elem.disabled) {
+      elem.classList.add("invalid-input");
+    }
+  });
+  let name = document.getElementById("name-input").value;
   if (name.trim() === "") {
     let label = document.getElementById("name-label");
     let attention = document.createElement("div");
     attention.className = "attention";
     attention.innerHTML = "обязательно к заполнению";
-    document.getElementById("name").classList.add("invalid-input");
+    document.getElementById("name-input").classList.add("invalid-input");
     label.classList.add("pink");
-    label.append(attention);
+    document.getElementsByClassName("attention").length < 1
+      ? label.append(attention)
+      : 0;
   }
 });
 
 document.querySelectorAll(".contact-selector").forEach(elem => {
   elem.addEventListener("click", () => {
-    console.log(elem.id);
     if (elem.id === "phone") {
       document.getElementById("contact-email").disabled = "disabled";
-      document.getElementById("email").disabled = "disabled";
-      document.getElementById("email-label").classList.add("disabled");
-      document.getElementById("phone-label").classList.add("pink");
+      document.getElementById("contact-phone").disabled = "";
     } else {
       document.getElementById("contact-phone").disabled = "disabled";
-      document.getElementById("phone").disabled = "disabled";
-      document.getElementById("phone-label").classList.add("disabled");
-      document.getElementById("email-label").classList.add("pink");
+      document.getElementById("contact-email").disabled = "";
     }
   });
 });
